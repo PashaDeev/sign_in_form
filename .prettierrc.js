e781040a329b7b6a2583c7a67a03ad86dcf9a400
pasha-deev <pasha-deev@yandex-team.ru>
2024-08-04 17:46:34 +0300

    configured linter and prettier

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+    printWidth: 120,
+    tabWidth: 4,
+    arrowParens: 'avoid',
+    bracketSameLine: false,
+    bracketSpacing: false,
+    endOfLine: 'lf',
+    semi: true,
+    singleQuote: true,
+    trailingComma: 'all',
+    useTabs: false,
+};
