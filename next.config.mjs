f713ff160229c703a05aa7e083ac85d72d277e4d
Pavel <34137983+PashaDeev@users.noreply.github.com>
2024-08-11 12:33:38 +0300

    Create nextjs.yml

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,19 @@
+import path from 'path';
+
+const __dirname = import.meta.dirname;
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     basePath: '/sign_in_form',
     output: 'export',
     reactStrictMode: true,
+    typescript: {
+        tsconfigPath: './tsconfig.json',
+    },
+    webpack(config) {
+        config.resolve.alias['@'] = path.join(__dirname, 'src');
+        return config;
+    },
 };
 
 export default nextConfig;
